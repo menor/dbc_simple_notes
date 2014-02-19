061247816d8d2b1d0939bd0af1d448189f9f6db0
menor <josemenorgmail.com>
2014-02-19 08:31:57 -0800

    Add a test for creating a new note

--- sinatra-skeleton-mvc/spec/index_controller_spec.rb
+++ sinatra-skeleton-mvc/spec/index_controller_spec.rb
@@ -1,30 +1,51 @@
 require 'spec_helper'
 
-describe "IndexController (this is a skeleton controller test!)" do
+describe "IndexController" do
 
-  describe 'get all bands' do
-    it 'should see all bands' do
-      get "/bands"
-      expect(last_response).to be_ok
-    end
+  it "should allow accessing the home page" do
+    get '/'
+    last_response.should be_ok
   end
 
-  describe 'create a new band' do
-    band_name = 'chromatics'
+  describe 'create a new note' do
+    new_note = 'Conquer Andorra'
     new_params = {
-        name: band_name
-      }
-    new_session = {
-      'rack.session' => {
-        # Could preload stuff into the session here...
-      }
+      note: new_note
     }
-    it 'should add a new band' do
+
+    it 'should add a new note' do
       expect{
-        post('/bands', new_params, new_session)
-      }.to change(Band, :count).by(1)
+        post('/notes', new_params)
+      }.to change(Note, :count).by(1)
       last_response.should be_ok
-      last_response.body.should == band_name
+      last_response.body.should == new_note
     end
   end
+
+
+  # describe 'get all bands' do
+  #   it 'should see all bands' do
+  #     get "/bands"
+  #     expect(last_response).to be_ok
+  #   end
+  # end
+
+  # describe 'create a new band' do
+  #   band_name = 'chromatics'
+  #   new_params = {
+  #       name: band_name
+  #     }
+  #   new_session = {
+  #     'rack.session' => {
+  #       # Could preload stuff into the session here...
+  #     }
+  #   }
+  #   it 'should add a new band' do
+  #     expect{
+  #       post('/bands', new_params, new_session)
+  #     }.to change(Band, :count).by(1)
+  #     last_response.should be_ok
+  #     last_response.body.should == band_name
+  #   end
+  # end
 end
